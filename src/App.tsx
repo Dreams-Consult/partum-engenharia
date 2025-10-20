ee1644900307bb6f842355d4a5b4f76fd59ca004
Caio Maia <caio.maiasb@gmail.com>
2025-10-20 09:49:52 -0300

    Adição do botão para o whatsapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import LogoWhatsapp from './assets/whatsapp-svgrepo-com.svg'
 import { Topbar } from './components/TopBar'
 import { Home } from './components/Home'
 import { AboutUs } from './components/AboutUs'
@@ -10,6 +11,9 @@ function App() {
 
   return (
     <>
+      <a href='https://wa.me/5517996542563?text=Olá+gostaria+de+saber+mais+sobre+o+serviço.' target='_blank' >
+        <img className='whatsapp-icon' src={LogoWhatsapp} alt='LogoWhatsapp' width="50" height="50" />
+      </a>
       <Topbar />
       <Home />
       <AboutUs />
